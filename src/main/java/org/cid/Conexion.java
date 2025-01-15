0324bb6edf754e2138f4b3446424eb3efb4551b8
cidesgo@gmail.com <cidesgo@gmail.com>
2025-01-15 11:04:01 -0600

    First commit

---
+++ src/main/java/org/cid/Conexion.java
@@ -0,0 +1,5 @@
+package org.cid;
+
+public class Conexion {
+
+}
---
+++ src/main/java/org/cid/Main.java
@@ -0,0 +1,7 @@
+package org.cid;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
