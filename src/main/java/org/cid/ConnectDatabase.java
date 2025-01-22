ddb1bc1163b022124b2bd3972fd4f7a592218f5f
cidesgo@gmail.com <cidesgo@gmail.com>
2025-01-22 10:33:07 -0600

    Refactorizando singleton para entornos multihilos

--- src/main/java/org/cid/ConnectDatabase.java
+++ src/main/java/org/cid/ConnectDatabase.java
@@ -6,24 +6,44 @@ import java.sql.SQLException;
 
 public class ConnectDatabase {
 
-    private static final String url = "jdbc:mysql://localhost:3306/mensajes_app";
-    private static final String user = "cidesgon";
-    private static final String pass = "Emmanuel";
+    private static final String URL = "jdbc:mysql://localhost:3306/mensajes_app";
+    private static final String USER = "cidesgon";
+    private static final String PASS = "Emmanuel";
 
-    private static Connection myConnection;
+    private static volatile Connection myConnection;
 
-    public static Connection getConnection() throws SQLException {
-        if (myConnection == null || myConnection.isClosed()){
-            try{
-                myConnection = DriverManager.getConnection(url, user, pass);
-                if (myConnection != null){
-                    System.out.println("Successfully Connected");
+    //ConstrcutorPrivado para evitar instancias
+    private ConnectDatabase(){}
+
+    public static Connection getConnection(){
+        if (myConnection == null){
+            synchronized (ConnectDatabase.class){
+                if (myConnection == null){ //Double check locking
+                    try {
+                        myConnection = DriverManager.getConnection(URL, USER, PASS);
+                        System.out.println("Successfully Connected");
+                    } catch (SQLException e) {
+                        throw new RuntimeException("Error al conectar con la base de datos", e);
+                    }
                 }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
             }
-
         }
         return  myConnection;
+    }
+
+    public static void closeConnection() {
+        if (myConnection != null) {
+            synchronized (ConnectDatabase.class) {
+                if (myConnection != null) {
+                    try {
+                        myConnection.close();
+                        myConnection = null; // Reiniciamos la conexión
+                        System.out.println("Conexión cerrada exitosamente");
+                    } catch (SQLException e) {
+                        throw new RuntimeException("Error al cerrar la conexión a la base de datos", e);
+                    }
+                }
+            }
+        }
     }
 }
